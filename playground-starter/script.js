b3deb31da3e5c5ee3a9880c5449166a2479b5e80
mvmirhan <mvmirhan@yahoo.com>
2022-07-23 21:51:34 +1000

    JS class notes

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,9 +1,47 @@
-function showName() {
-    let inputField = document.getElementById('name-text')
-    // variable name best practices
-    // console.log("The Input Field Is: ", inputField);
-    // console.dir(inputField) - use dir to get all the available options
-    alert ("Hello " + inputField.value)
-}
+
+let pets = ["Chilli","Rex","Roary","Fleur","Bella"];
+
+pets.forEach((pet) => {
+    if (pet == "Bella") {
+        console.log("I found Bella", pet);
+    }  else if (pet == "Chilli") {
+        console.log("It's Chilli this time");
+    }  else {
+        console.log("All of them are cute")
+    }
+});
+
+
+// for (let index = 0; index < pets.length; index++) {
+//     console.log("Found Pet:", pets[index]);
+// }
+
+// pets.forEach((pet) => {
+//     console.log("For each | Found Pet", pet);
+// });
+
+
+// console.log("Hello World")
+
+// for (let index = 0; count < 10; count++) {
+//     console.log("Found Count", count);
+// }
+
+// pets.pop()
+// // pets.splice()
+// pets.push("Bella")
+
+// console.log("Found Pets", pets)
+
+
+// function showName() {
+//     let inputField = document.getElementById('name-text')
+//     // variable name best practices
+//     // console.log("The Input Field Is: ", inputField);
+//     // console.dir(inputField) - use dir to get all the available options
+//     alert ("Hello " + inputField.value)
+// }
+
+
 
 
--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,3 +1,46 @@
-function addItem() {}
+// let shoppingListItems = []
+let shoppingListItems = ["milk","eggs","bread"]
+let listElement = document.getElementById("shopping-list-items");
 
-function updateItems() {}
+
+updateItems()
+
+function updateItems() {
+    let listElement = document.getElementById("shopping-list-items");
+    listElement.innerHTML = "";
+    
+    shoppingListItems.forEach((item) => {
+        let itemElement = document.createElement("li");
+        itemElement.innerText = item;
+        listElement.appendChild(itemElement);});
+}
+
+function addItem() {
+    let item = document.getElementById("new-item-text").value;
+    shoppingListItems.push(item);
+    updateItems()
+    console.log(shoppingListItems);
+}
+
+function clearItems() {
+    shoppingListItems=[]
+    updateItems()
+}
+
+// shoppingListItems.forEach((item) => {
+//     let itemElement = document.createElement("ul");
+//     itemElement.innerText = item;
+//     listElement.appendChild(itemElement);
+//     console.log(item)
+// });
+
+// function updateItems() {
+//     let listElement = document.getElementById("shopping-list-items");
+//     listElement.innerHTML = "";
+    
+//     shoppingListItems.forEach((item) => {
+//         let itemElement = document.createElement("li");
+//         itemElement.innerText = item;
+//         listElement.appendChild(itemElement);});
+// }
+
